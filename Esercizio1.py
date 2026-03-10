ef24ac3a4ae1ec9882f746d79acaaa468f7279d9
beatrice063 <beatricezavattin@gmail.com>
2026-03-10 16:51:31 +0000

    lezione 3

---
+++ Esercizio1.py
@@ -0,0 +1,28 @@
+def is_pari(n):
+    """Ritorna vero se "n" è pari, se no ritorna falso """
+
+    risultato = False
+
+    if n%2 == 0 :
+        risultato = True
+
+
+    return risultato
+
+#####
+
+main():
+    numero = int( input ('Dammi un numero:') )
+
+    #print(type(numero))
+
+    result = is_pari(numero)
+
+    print(result)
+
+print(result)
+
+
+
+
+
